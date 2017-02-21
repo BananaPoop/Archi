4af262ead00d440f138943f81989d3ac247297a5
BananaPoop <bananapoop237@gmail.com>
2017-02-21 16:26:11 +0800

    misc

--- Archi/Program.cs
+++ Archi/Program.cs
@@ -3,12 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using OpenTK;
 
 namespace Archi
 {
     class Program
     {
         static void Main(string[] args) {
+            using (Game game = new Game()) {
+                game.Run(60.0);
+            }
         }
     }
 }
